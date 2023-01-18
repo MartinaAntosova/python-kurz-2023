5db7a15774d9f3a086675a8cf2aabceefc7b1ade
Martina Antosova <martina.antosova@post.cz>
2023-01-18 18:20:51 +0100

    Ukol-01

---
+++ ukol-01-bonus.py
@@ -0,0 +1,11 @@
+jmeno_a_prijmeni = input("Zadejte prosím jméno a příjmení: ")
+
+# Vypiš u jména a příjmení všechna písmena velká.
+print(str.upper(jmeno_a_prijmeni))
+
+# Vypiš u jména a příjmení všechna písmena malá.
+print(str.lower(jmeno_a_prijmeni))
+
+# Vypiš u jména a příjmení první písmena velká, další malá.
+print(str.title(jmeno_a_prijmeni))
+
---
+++ ukol-01.py
@@ -0,0 +1,4 @@
+jmeno = "Martina"
+
+# Vypiš e-mailovou adresu se jménem a doménou czechitas.cz.
+print(jmeno + "@czechitas.cz")
