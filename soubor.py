ca1d924ce9e10f77c098792dea53bc2a1bde4bf4
Martina Antosova <martina.antosova@post.cz>
2023-02-21 19:14:15 +0100

    ukol-03_oprava

--- soubor.py
+++ soubor.py
@@ -2,13 +2,17 @@ import json
 with open('body.json', encoding='utf-8') as soubor:
     body = json.load(soubor)
 
+slovnik_prospech = {}
+
 bodova_hranice = 60
 
 for key, value in body.items():
     if value >= bodova_hranice:
         print(f"{key}: Pass")
+        slovnik_prospech[key] = "Pass"
     else: 
-        print(f"{key}: Fail") 
+        print(f"{key}: Fail")
+        slovnik_prospech[key] = "Fail" 
 
-with open('prospech.json', mode='w', encoding='utf-8') as soubor:
-    json.dump(body, soubor)
+with open('slovnik_prospech.json', mode='w', encoding='utf-8') as soubor:
+    json.dump(slovnik_prospech, soubor)
